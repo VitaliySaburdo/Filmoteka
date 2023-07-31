30fb597b800aee281fa15c7e53dca937082d6b54
VitaliySaburdo <vsaburdo@gmail.com>
2023-07-31 22:03:55 +0300

    add markup library

--- src/js/library-page.js
+++ src/js/library-page.js
@@ -1,9 +1,9 @@
 import {
   addToStorage,
   getFromStorage,
-  removeFromStorage,
 } from './local-storage';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import {renderLibrary} from './markup-library';
 
 export const libraryEl = document.querySelector('.library__list');
 const queueButton = document.querySelector('.queue_button');
@@ -57,8 +57,6 @@ export function libraryStorage(movieData) {
     if (movieData) {
       const movieList = type === 'watch' ? watchMovie : queueMovie;
       const movieIndex = movieList.findIndex(e => e.id === movieData.id);
-
-      console.log(movieIndex);
 
       if (movieIndex !== -1) {
         button.classList.remove('button--accent');
@@ -124,61 +122,8 @@ function handleClickQueue() {
 function renderSavedFilms(name) {
   const storageMovies = getFromStorage(name);
   if (storageMovies) {
-    renderLibrary(storageMovies, name);
+    renderLibrary(storageMovies);
   } else {
-    renderLibrary([], name);
+    renderLibrary([]);
   }
-}
-
-function renderLibrary(data, name) {
-  const markup = data
-    .map(
-      ({
-        id,
-        poster_path,
-        title,
-        genres,
-        release_date,
-      }) => `<li class="gallery__item" data-id="${id}">
-        <img src="${renderImg(
-          poster_path
-        )}" alt="${title}" class="gallery_img" width="395" height="574" />
-        <h2 class="gallery__title">${title}</h2>
-        <p class="gallery__txt">${genresConverting(
-          genres
-        )} | ${release_date.slice(0, 4)}
-        </li>`
-    )
-    .join('');
-
-  if (libraryEl) {
-    libraryEl.innerHTML =
-      markup ||
-      (localStorage.getItem('lang') === 'en'
-        ? `<div class="library__background">
-      <h2 class='library__title' data-lang='library__title'>Sorry, there are no attached movies here</h2>
-      </div>`
-        : `<div class="library__background">
-      <h2 class='library__title' data-lang='library__title'>Нажаль тут немає доданих фільмів</h2>
-      </div>`);
-  }
-}
-
-function renderImg(poster_path) {
-  if (poster_path) {
-    return `https://image.tmdb.org/t/p/w500${poster_path}`;
-  }
-  return `https://upload.wikimedia.org/wikipedia/commons/c/c2/No_image_poster.png?20170513175923`;
-}
-
-function genresConverting(genres) {
-  if (genres.length) {
-    const genreArray = [];
-    genres.map(genre => {
-      genreArray.push(genre.name);
-    });
-
-    return genreArray.join(', ');
-  }
-  return 'N/A';
 }
---
+++ src/js/markup-library.js
@@ -0,0 +1,54 @@
+const libraryEl = document.querySelector('.library__list');
+
+export function renderLibrary(data) {
+  const markup = data
+    .map(
+      ({
+        id,
+        poster_path,
+        title,
+        genres,
+        release_date,
+      }) => `<li class="gallery__item" data-id="${id}">
+        <img src="${renderImg(
+          poster_path
+        )}" alt="${title}" class="gallery_img" width="395" height="574" />
+        <h2 class="gallery__title">${title}</h2>
+        <p class="gallery__txt">${genresConverting(
+          genres
+        )} | ${release_date.slice(0, 4)}
+        </li>`
+    )
+    .join('');
+
+  if (libraryEl) {
+    libraryEl.innerHTML =
+      markup ||
+      (localStorage.getItem('lang') === 'en'
+        ? `<div class="library__background">
+      <h2 class='library__title' data-lang='library__title'>Sorry, there are no attached movies here</h2>
+      </div>`
+        : `<div class="library__background">
+      <h2 class='library__title' data-lang='library__title'>Нажаль тут немає доданих фільмів</h2>
+      </div>`);
+  }
+}
+
+function renderImg(poster_path) {
+  if (poster_path) {
+    return `https://image.tmdb.org/t/p/w500${poster_path}`;
+  }
+  return `https://upload.wikimedia.org/wikipedia/commons/c/c2/No_image_poster.png?20170513175923`;
+}
+
+function genresConverting(genres) {
+  if (genres.length) {
+    const genreArray = [];
+    genres.map(genre => {
+      genreArray.push(genre.name);
+    });
+
+    return genreArray.join(', ');
+  }
+  return 'N/A';
+}
